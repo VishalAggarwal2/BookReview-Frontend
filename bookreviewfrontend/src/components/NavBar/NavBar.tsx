fdfb05aea2cff2f3fb85d32c3fb6c74670f865a1
VishalAggarwal2 <vishalaggarwal270@gmail.com>
2024-06-28 02:38:38 +0530

    make navbar sticky

--- bookreviewfrontend/src/components/NavBar/NavBar.tsx
+++ bookreviewfrontend/src/components/NavBar/NavBar.tsx
@@ -6,13 +6,12 @@ import {
     SignInButton,
     SignedIn,
     SignedOut,
-  } from '@clerk/nextjs';
+} from '@clerk/nextjs';
 import NavbarButton from '../NavBarButton/NavbarButton';
 
 const NavBar = () => {
   return (
-    <div 
-className='flex flex-row justify-between bg-red-950  text-white p-7 align-middle'>
+    <div className='sticky top-0 z-50 flex flex-row justify-between bg-red-950 text-white p-7 align-middle'>
       <h1 className='text-2xl'>
         <Link href={"/"}>
           BookReview
@@ -24,11 +23,11 @@ className='flex flex-row justify-between bg-red-950  text-white p-7 align-middle
         </SignedOut>
         <SignedIn>
           <div className='flex flex-row items-center gap-3 sm-md:hidden'>
-            <Link className=' bg-white text-red-950 border-10 p-1' style={{borderRadius:"10px"}} href={"/about"}>About</Link>
-            <Link className=' bg-white text-red-950 border-10 p-1' style={{borderRadius:"10px"}} href={"/AddBookReview"}>AddBookReview</Link>
-            <Link className=' bg-white text-red-950 border-10 p-1' style={{borderRadius:"10px"}} href={"/ShowPastReview"}>ShowPastReview</Link>
-            <Link className=' bg-white text-red-950 border-10 p-1' style={{borderRadius:"10px"}} href={"/AllBookReview"}>AllBookReview</Link>
-            <Link className=' bg-white text-red-950 border-10 p-1' style={{borderRadius:"10px"}} href={"/devlopers"}>Developer</Link>
+            <Link className=' bg-white text-red-950 p-1' style={{borderRadius: "10px"}} href={"/about"}>About</Link>
+            <Link className=' bg-white text-red-950 p-1' style={{borderRadius: "10px"}} href={"/AddBookReview"}>AddBookReview</Link>
+            <Link className=' bg-white text-red-950 p-1' style={{borderRadius: "10px"}} href={"/ShowPastReview"}>ShowPastReview</Link>
+            <Link className=' bg-white text-red-950 p-1' style={{borderRadius: "10px"}} href={"/AllBookReview"}>AllBookReview</Link>
+            <Link className=' bg-white text-red-950 p-1' style={{borderRadius: "10px"}} href={"/developers"}>Developer</Link>
             <UserButton />
           </div>
           <div className='block lg:hidden'>
